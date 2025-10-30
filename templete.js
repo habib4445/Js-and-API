584cc857b257b0d9634242650ebd0d4c1ed6aa3c
habib4445 <0124habibur@gmail.com>
2025-10-30 22:24:06 +0600

    add another

--- templete.js
+++ templete.js
@@ -1,3 +1,7 @@
-const countryName="Bangladesh";
-const anthem=`amar sonar ${countryName} ami tomay valobashi `
-console.log(anthem);
+// const countryName="Bangladesh";
+// const anthem=`amar sonar ${countryName} ami tomay valobashi `
+// console.log(anthem);
+const sum=(num1,num2)=>{
+    return num1+num2;
+};
+console.log(sum(10,20));
