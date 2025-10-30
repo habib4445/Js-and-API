93b4b613f9bc235dee215737e8908e6b3f02ee09
habib4445 <0124habibur@gmail.com>
2025-10-31 00:29:05 +0600

    add

--- array-des.js
+++ array-des.js
@@ -1,27 +1,35 @@
-const habib={
+// const habib={
+//     age:25,
+//     name:'habib',
+//     status:'not found '
+// }
+// const {name,age,status}=habib;
+// console.log(age);
+
+// const person=["habib","shakib","Ebru"];
+// const [hab,tab,sab]=person;
+// console.log(tab,sab,hab);
+
+// const ha={
+//     age:25,
+//     name:'habib',
+//     status:'not found ',
+// }
+// console.log(Object.values(ha));
+
+// const person1={
+//     age:25,
+//     name:'habib',
+//     status:'not found ',
+// }
+// for(let key of Object.entries[person1]){
+//     console.log(key);
+// }
+const person1={
     age:25,
     name:'habib',
     status:'not found '
 }
-const {name,age,status}=habib;
-console.log(age);
-
-const person=["habib","shakib","Ebru"];
-const [hab,tab,sab]=person;
-console.log(tab,sab,hab);
-
-const ha={
-    age:25,
-    name:'habib',
-    status:'not found ',
-}
-console.log(Object.values(ha));
-
-const h={
-    age:25,
-    name:'habib',
-    status:'not found ',
-}
-for(let key in h){
-    console.log(` key: ${key} value: ${h}[key]`);
+for(let [key,value] of Object.entries(person1)){
+    console.log(`key ${key} value: ${value}`);
 }
