b4869c014b4c6ba22ccfac10870a4a8c49d9a048
habib4445 <0124habibur@gmail.com>
2025-10-30 23:26:57 +0600

    array dec

---
+++ array-des.js
@@ -0,0 +1,11 @@
+const habib={
+    age:25,
+    name:'habib',
+    status:'not found '
+}
+const {name,age,status}=habib;
+console.log(age);
+
+const person=["habib","shakib","Ebru"];
+const [hab,tab,sab]=person;
+console.log(tab,sab,hab);
--- spread.js
+++ spread.js
@@ -1,3 +1,6 @@
-const friends=["habib","shofiq","rafiq"];
-const newArray=["hero alom", ...friends];
-console.log(newArray);
+// const friends=["habib","shofiq","rafiq"];
+// const newArray=["hero alom", ...friends];
+// console.log(newArray);
+
+const numbers=[2,4,56,89,4,54,653546,5,5,4546,334,2,33233434,56343,34];
+console.log(Math.min(...numbers));
