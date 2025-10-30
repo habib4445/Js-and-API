4bb9fa33e6e2eeac66c59b7b9a9468eb3cf5db50
habib4445 <0124habibur@gmail.com>
2025-10-31 00:19:57 +0600

    docueent

--- array-des.js
+++ array-des.js
@@ -9,3 +9,19 @@ console.log(age);
 const person=["habib","shakib","Ebru"];
 const [hab,tab,sab]=person;
 console.log(tab,sab,hab);
+
+const ha={
+    age:25,
+    name:'habib',
+    status:'not found ',
+}
+console.log(Object.values(ha));
+
+const h={
+    age:25,
+    name:'habib',
+    status:'not found ',
+}
+for(let key in h){
+    console.log(` key: ${key} value: ${h}[key]`);
+}
