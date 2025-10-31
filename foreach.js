fdf7d879c788cb32e0e793c119d03fc16a614976
habib4445 <0124habibur@gmail.com>
2025-10-31 21:04:04 +0600

    add

--- foreach.js
+++ foreach.js
@@ -1,10 +1,14 @@
  const products=[
-    {id:1, name:"iphone",color:"black", price:1200, brand:"apple"},
-    {id:2, name:"xiaomi",color:"gold", price:100, brand:"apple"},
-    {id:3, name:"samsung",color:"gold", price:1500, brand:"apple"},
-    {id:4, name:"iphone",color:"gold", price:1000, brand:"apple"},
+    {id:1, name:"iphone",color:"black", price:1200, brand:"applet"},
+    {id:2, name:"xiaomi",color:"gold", price:100, brand:"applel"},
+    {id:3, name:"samsung",color:"gold", price:1500, brand:"apples"},
+    {id:4, name:"iphone",color:"gold", price:1000, brand:"apples"},
     {id:5, name:"xiaomi",color:"black", price:1100, brand:"apple"},
  ];
- products.forEach(product => {
-    console.log(product)
- })
+const newProducts=products.map(p=>{
+    if(p.band==="apple"){
+        p.price=p.price+100;
+    }
+    return p;
+})
+console.log(newProducts);
