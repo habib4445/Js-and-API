167cf01a93ce89ef5133f0b7a23192145fcca9b2
habib4445 <0124habibur@gmail.com>
2025-10-31 09:16:56 +0600

    nested

--- nested.js
+++ nested.js
@@ -1,10 +1,15 @@
 const person={
     name:"habib",
     age:20,
+    1:"100",
     friends:["sakib","rafiq"],
     details:{
         job:"yes",
         ismarried:true,
+        father: {
+            name: "Mostofa",
+        }
     }
 }
-console.log(person.friends[1])
+console.log(person[1]);
+// console.log(person.details.father.name);
