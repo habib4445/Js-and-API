9f726dfcfe930b7e65745e3fba5accaf075d0fb8
habib4445 <0124habibur@gmail.com>
2025-10-31 20:30:45 +0600

    foreach

--- arraymap.js
+++ arraymap.js
@@ -3,6 +3,15 @@
 // console.log(newArray);
 
 
-const numbers=[1,2,3,4,5,6,7,];
-const squareArray=numbers.map(element=>element*element);
-console.log(squareArray);
+// const numbers=[1,2,3,4,5,6,7,];
+// const squareArray=numbers.map(element=>element*element);
+// console.log(squareArray);
+
+const friends=['habib','shakib','masum','rabbi'];
+const newfriends=friends.map((element,index)=>
+{
+    console.log(index),
+console.log(element)
+}
+
+)
---
+++ foreach.js
@@ -0,0 +1,10 @@
+ const products=[
+    {id:1, name:"iphone",color:"black", price:1200, brand:"apple"},
+    {id:2, name:"xiaomi",color:"gold", price:100, brand:"apple"},
+    {id:3, name:"samsung",color:"gold", price:1500, brand:"apple"},
+    {id:4, name:"iphone",color:"gold", price:1000, brand:"apple"},
+    {id:5, name:"xiaomi",color:"black", price:1100, brand:"apple"},
+ ];
+ products.forEach(product => {
+    console.log(product)
+ })
