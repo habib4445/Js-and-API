46e767f605312763f43367c2352b0e8ae15b644d
habib4445 <0124habibur@gmail.com>
2025-10-31 10:16:14 +0600

    arraymap

---
+++ arraymap.js
@@ -0,0 +1,8 @@
+// const numbers=[1,2,3,4,5,6,7,];
+// const newArray=numbers.map(number=>number+1);
+// console.log(newArray);
+
+
+const numbers=[1,2,3,4,5,6,7,];
+const squareArray=numbers.map(element=>element*element);
+console.log(squareArray);
